eef672ce0b61af1066e7c260c0097e8ba5c6d52f
Fedor Resnyanskiy <fa.resnyanskiy@jet.msk.su>
2019-11-26 21:04:05 +0300

    add archetype for default bpm project

---
+++ archetype/src/main/resources/archetype-resources/itest/src/test/java/OSGiTestEnvironment.java
@@ -0,0 +1,38 @@
+package ${package};
+
+import org.ops4j.pax.exam.ConfigurationFactory;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Option;
+
+import java.io.File;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+
+/**
+ * @author Fedor Resnyanskiy
+ */
+public class OSGiTestEnvironment implements ConfigurationFactory {
+
+    @Override
+    public Option[] createConfiguration() {
+        return options(
+            karafDistributionConfiguration().frameworkUrl(
+                maven().groupId("${groupId}")
+                    .artifactId("${rootArtifactId}-assembly")
+                    .type("zip")
+                    .versionAsInProject())
+                .useDeployFolder(false)
+                .unpackDirectory(new File("target/exam/")),
+            features("fake", "bpm-default-engine"),
+            keepRuntimeFolder(),
+//          logLevel(LogLevelOption.LogLevel.TRACE),
+            doNotModifyLogConfiguration(),
+//          debugConfiguration("5005", true),
+            CoreOptions.junitBundles()
+        );
+    }
+
+
+}
